a9b6be3b065a5a62bfe2fce48d6020586b939e88
Jasmin Gerhardt <jasmin.gerhardt89@gmail.com>
2022-09-25 18:22:21 +0200

    finished task

--- Lab 3.08..sql
+++ Lab 3.08..sql
@@ -1,34 +1,22 @@
 USE sakila;
 
--- 1. Create a query or queries to extract the information you think may be relevant for building the prediction model
--- It should include some film features and some rental features.
--- Use the data from May 2005.
-
 SELECT * FROM rental;
 SELECT * FROM film;
 
 SELECT
+	f.film_id,
 	f.title,
+    f.rental_duration,
     l.name AS 'language',
     f.rental_rate,
     f.length,
     f.rating,
     f.special_features,
     c.name AS 'category',
-    r.rental_date
-FROM film f
+    MONTH(r.rental_date) AS month
+    FROM film f
 JOIN film_category ca USING(film_id)
 JOIN category c USING(category_id)
-JOIN inventory i USING(film_id)
 JOIN language l USING(language_id)
-JOIN rental r USING(inventory_id)
-WHERE r.rental_date BETWEEN '2005-05-01' AND '2005-05-31';
-    
-
-
-
-
-
-
-
-
+JOIN inventory i USING(film_id)
+JOIN rental r USING(inventory_id);
